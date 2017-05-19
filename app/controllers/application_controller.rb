6d0e16573f9b86afaec255350d8478dc57881739
danielmoralesp <daniel_moralesp@hotmail.com>
2017-05-18 23:47:38 -0500

    Install Devise with Model and Migrations, and Role Column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :add_parameters_to_user, if: :devise_controller?
+
+  def add_parameters_to_user
+    devise_parameter_sanitizer.for(:sign_up) << :role
+    devise_parameter_sanitizer.for(:account_update) << :role
+  end
 end
