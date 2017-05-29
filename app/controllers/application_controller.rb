8561406404a9f5d1d0746ddffa4e6fde271b6d7c
danielmoralesp <daniel_moralesp@hotmail.com>
2017-05-29 18:48:13 -0500

    header and footer dinamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #before_action :add_parameters_to_user, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #def add_parameters_to_user
-  #  devise_parameter_sanitizer.for(:sign_up) << :role
-  #  devise_parameter_sanitizer.for(:account_update) << :role
-  #end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
 end
